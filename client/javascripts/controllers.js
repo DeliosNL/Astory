14ef13069aabd03c146f99d5b8557526fe75e0c0
Niels <nielmaster@gmail.com>
2013-12-10 14:42:11 +0100

    Assets ophalen gaat nu met een JSON object + wat CSS wijzigingen

--- client/javascripts/controllers.js
+++ client/javascripts/controllers.js
@@ -3,7 +3,97 @@
 
 "use strict";
 
-aStory.controller('mainController', ['$scope',  function ($location, $scope, $http) {
+aStory.controller('mainController', ['$scope', function ($location, $scope, $http) {
 
 
+}]);
+
+aStory.controller('editorController', ['$scope', function ($scope) {
+    $scope.assetgroups = [
+        {
+            name: "Most used assets",
+            assets: [
+                {
+                    "name": "Kerstboom",
+                    "description": "Kerstboom",
+                    "image": "Boom.png"
+                },
+                {
+                    "name": "Hond",
+                    "description": "Keeshond",
+                    "image": "Hond.png"
+                },
+                {
+                    "name": "Rendier",
+                    "description": "Rendier",
+                    "image": "Rendier.png"
+                },
+                {
+                    "name": "Sneeuwpop",
+                    "description": "Sneeuwpop",
+                    "image": "Sneeuwpop.png"
+                },
+                {
+                    "name": "Achtergrond1A",
+                    "description": "Sneeuwbergen achtergrond",
+                    "image": "Achtergrond1A.png"
+                },
+                {
+                    "name": "Spaceshuttle",
+                    "description": "Spaceshuttle",
+                    "image": "Spaceshuttle.png"
+                }
+            ]
+        },
+        {
+            name: "Characters",
+            assets: [
+                {
+                    "name": "Hond",
+                    "description": "Keeshond",
+                    "image": "Hond.png"
+                },
+                {
+                    "name": "Rendier",
+                    "description": "Rendier",
+                    "image": "Rendier.png"
+                },
+                {
+                    "name": "Sneeuwpop",
+                    "description": "Sneeuwpop",
+                    "image": "Sneeuwpop.png"
+                }
+            ]
+        },
+        {
+            name: "Huizen",
+            assets: [
+                {
+                    "name": "Wit huisje",
+                    "description": "Wit huisje",
+                    "image": "Huis1.png"
+                },
+                {
+                    "name": "Wit huisje 2",
+                    "description": "Wit huisje v2",
+                    "image": "Huis2.png"
+                },
+                {
+                    "name": "Bruin huisje",
+                    "description": "Bruin huisje",
+                    "image": "Huis3.png"
+                },
+                {
+                    "name": "Bruin huisje 2",
+                    "description": "Bruin huisje v2",
+                    "image": "Huis4.png"
+                },
+                {
+                    "name": "Mooi huis",
+                    "description": "Mooi huisje",
+                    "image": "6_huis.png"
+                }
+            ]
+        }
+    ];
 }]);
