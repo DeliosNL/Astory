ab9956640a6b3007075fb1ab2b87cf064bca0b0e
Niels <nielmaster@gmail.com>
2013-12-19 16:45:46 +0100

    Editor breedte fix + editorbar knoppen met een after + ellipsis

--- client/javascripts/directives.js
+++ client/javascripts/directives.js
@@ -1,10 +1,26 @@
 aStory.directive('assetimage', function () {
     "use strict";
     return {
-        link: function(scope, element, attrs) {
-            element.bind("load", function(e) {
+        link: function (scope, element, attrs) {
+            element.bind("load", function (e) {
                 element[0].addEventListener('dragstart', scope.dragAsset);
             });
         }
+    };
+});
+
+aStory.directive('accountdropdown', function () {
+    "use strict";
+    return {
+        restrict: 'E',
+        templateUrl: '../partials/directives/accountdropdown.html'
+    };
+});
+
+aStory.directive('scenariosdropdown', function() {
+    "use strict";
+    return {
+        restrict: 'E',
+        templateUrl: '../partials/directives/scenariosdropdown.html'
     };
 });
