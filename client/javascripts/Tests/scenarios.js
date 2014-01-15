3dd1f268df74286bf68d94d3c89607fc6ec659da
Edwin <edwinvdr@hotmail.com>
2014-01-15 15:04:23 +0100

    Extra E2E tests

--- client/javascripts/Tests/scenarios.js
+++ client/javascripts/Tests/scenarios.js
@@ -15,53 +15,6 @@ var logout = function() {
     element(".accountdropdownbuttonlogout").click();
 };
 
-describe('editor', function () {
-    beforeEach(function () {
-        login();
-        element('.editbutton').click();
-    });
-
-    afterEach(function () {
-        logout();
-    });
-
-    it("Shows the scenario dropdown when clicked", function () {
-        expect(element('.editorbardropdown').css('display')).toBe('none');
-        element("#scenariobutton").click();
-        sleep(1);
-        expect(element('.editorbardropdown').css('display')).toBe('block');
-        element("#scenariobutton").click();
-        sleep(1);
-        expect(element('.editorbardropdown').css('display')).toBe('none');
-    });
-
-});
-
-describe('login', function () {
-    beforeEach(function () {
-        browser().navigateTo('/#/login');
-    });
-
-    it("Recognizes invalid login details and shows an error", function () {
-        expect(element('.errormessage').css('display')).toBe('none');
-        input('username').enter("onzin124124121j25i1j25125125ioj12");
-        input('password').enter("onzina3r23ar32523a5a2");
-        element("#loginbutton").click();
-        sleep(1);
-        expect(element('.errormessage').css('display')).toBe('block');
-    });
-
-    it("Recognizes valid login details", function () {
-        input('username').enter("testaccount");
-        input('password').enter("testtest");
-        element("#loginbutton").click();
-        sleep(2);
-        expect(browser().location().url()).toBe('/stories');
-        logout();
-    });
-
-});
-
 describe('Story overview', function () {
     beforeEach(function () {
         login();
@@ -71,6 +24,47 @@ describe('Story overview', function () {
 
     afterEach(function () {
         logout();
+    });
+
+    var oldStoryCount = -2;
+
+    it("is checking the amount of stories", function () {
+        element('#ng-view').query(function (el, done) {
+            oldStoryCount = el.children().length - 1;
+            done();
+        });
+    });
+
+    it("Can create a new story", function () {
+        element('#newstorybutton').click();
+        sleep(2);
+        input('newstoryname').enter('test');
+        element('.bottomrightbutton').click();
+        expect(repeater('.story').count()).toMatch(oldStoryCount + 1);
+    });
+
+    it("Can delete a story", function () {
+        element(".storysettings").click();
+        sleep(2);
+        element(".bottomleftredtext").click();
+        sleep(2);
+        element(".bottomrightbutton").click();
+        sleep(2);
+    });
+
+    it("Can hide and show the account dropdown menu", function () {
+        expect(element('#accountdropdown').css('display')).toBe('none');
+        element("#navdropdownbutton").click();
+        sleep(1);
+        expect(element('#accountdropdown').css('display')).toBe('block');
+        element("#navdropdownbutton").click();
+        sleep(1);
+        expect(element('#accountdropdown').css('display')).toBe('none');
+    });
+
+    it("Home button should send you to the homepage", function () {
+        element('.navlogo').click();
+        expect(browser().location().url()).toBe('/home');
     });
 
     it("Should redirect a logged-out user", function () {
@@ -99,6 +93,108 @@ describe('Story overview', function () {
         element(".viewbutton").click();
         sleep(2);
         expect(browser().location().url()).toBe('/preview');
+    });
+
+});
+
+describe('editor', function () {
+    beforeEach(function () {
+        login();
+        element('.editbutton').click();
+    });
+
+    afterEach(function () {
+        logout();
+    });
+
+    var oldSceneCount = -2,
+        oldScenarioCount = -2;
+
+    it("is checking the amount of scenes and scenarios", function () {
+        element('#scenemenu').query(function (el, done) {
+            oldSceneCount = el.children().length;
+            done();
+        });
+
+        element('#scenarioswrapper').query(function (el, done) {
+            oldScenarioCount = el.children().length;
+            done();
+        });
+    });
+
+    it("Can hide and show an asset category", function () {
+        expect(element('.assetscontainer:eq(0)').css('display')).toBe('block');
+        element(".assetgroupheader:eq(0)").click();
+        sleep(1);
+        expect(element('.assetscontainer:eq(0)').css('display')).toBe('none');
+        element(".assetgroupheader:eq(0)").click();
+        sleep(1);
+        expect(element('.assetscontainer:eq(0)').css('display')).toBe('block');
+    });
+
+    it("Adds a scene when you click the button to add a scene", function () {
+        element('#plus').click();
+        expect(repeater('.sceneitem').count()).toMatch(oldSceneCount + 1);
+    });
+
+    it("Can open the preview page", function () {
+        element('#previewbutton').click();
+        expect(browser().location().url()).toBe('/preview');
+    });
+
+    it("Is able to add a new scenario", function () {
+        element("#scenariobutton").click();
+        sleep(1);
+        element('.newscenariobutton').click();
+        sleep(2);
+        input('newscenarioname').enter('test');
+        element('.bottomrightbutton').click();
+        expect(repeater('.scenariowrapper').count()).toBe(oldScenarioCount + 1);
+        sleep(1);
+        element("#scenariobutton").click();
+    });
+
+    it("Should be able to change a story name", function () {
+        element(".storysettingsbutton").click();
+        sleep(2);
+        input('storyname').enter('test');
+        element(".bottomrightbutton").click();
+        sleep(2);
+        expect(element('.storysettingsbutton').text()).toBe('\n            Story: test\n        ');
+    });
+
+    it("Shows the scenario dropdown when clicked", function () {
+        expect(element('.editorbardropdown').css('display')).toBe('none');
+        element("#scenariobutton").click();
+        sleep(1);
+        expect(element('.editorbardropdown').css('display')).toBe('block');
+        element("#scenariobutton").click();
+        sleep(1);
+        expect(element('.editorbardropdown').css('display')).toBe('none');
+    });
+});
+
+describe('login', function () {
+    beforeEach(function () {
+        browser().navigateTo('/#/login');
+    });
+
+    it("Recognizes invalid login details and shows an error", function () {
+        expect(element('.errormessage').css('display')).toBe('none');
+        input('username').enter("onzin124124121j25i1j25125125ioj12");
+        input('password').enter("onzina3r23ar32523a5a2");
+        element("#loginbutton").click();
+        sleep(1);
+        expect(element('.errormessage').css('display')).toBe('block');
+    });
+
+    it("Recognizes valid login details", function () {
+        input('username').enter("testaccount");
+        input('password').enter("testtest");
+        element("#loginbutton").click();
+        sleep(2);
+        expect(browser().location().url()).toBe('/stories');
+        logout();
     });
 
 });
